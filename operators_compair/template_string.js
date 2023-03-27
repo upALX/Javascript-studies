a0e3827898aabd4d9654a4867dd68d177336bcfb
José Alexandre <jose.antunes@qitech.com.br>
2023-03-26 23:08:18 -0300

    adding operators class into the project

---
+++ operators_compair/template_string.js
@@ -0,0 +1,5 @@
+const theName = "Alexandre"
+
+const presentation = `Ola, meu nome e ${theName}` 
+
+console.log(presentation)
---
+++ operators_compair/ternario.js
@@ -0,0 +1,4 @@
+let minimumAge = 18 
+let age = 19
+
+console.log(age >= minimumAge ? "Pode beber" : "Não pode beber alcool") // if inline ou operador ternário. Térnario porque tem 3 operadores de comparação.
